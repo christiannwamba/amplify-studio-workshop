168d51973384cf3bea97ad42900da204f99a78f6
Christian Nwamba <nwambachristian@gmail.com>
2023-03-27 18:29:19 +0100

    Pre-populate edit form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { View, Flex } from "@aws-amplify/ui-react";
 import React from "react";
-import { PostCardCollection, PostCreateForm, PostUpdateForm } from './ui-components'
-import Modal from './components/Modal'
+import {
+  PostCardCollection,
+  PostCreateForm,
+  PostUpdateForm,
+} from "./ui-components";
+import Modal from "./components/Modal";
 
 export default function App() {
-  const [isModalOpen, setIsModalOpen] = React.useState(false)
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
+  const [post, setPost] = React.useState({});
+
   return (
     <View width="100%">
       <Flex justifyContent="center" alignItems="center" direction="column">
@@ -15,23 +21,30 @@ export default function App() {
             </View>
           </Flex>
           <View marginTop="32px">
-            <PostCardCollection overrideItems={({item}) => {
-              return {
-                overrides: {
-                  Button36852910: {
-                    onClick: () => {
-                      setIsModalOpen(true)
-                    }
-                  }
-                }
-              }
-            }} />
+            <PostCardCollection
+              overrideItems={({ item }) => {
+                return {
+                  overrides: {
+                    Button36852910: {
+                      onClick: () => {
+                        setIsModalOpen(true);
+                        setPost(item);
+                      },
+                    },
+                  },
+                };
+              }}
+            />
           </View>
         </View>
       </Flex>
 
-      <Modal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} title="Edit Post">
-        <PostUpdateForm />
+      <Modal
+        isModalOpen={isModalOpen}
+        setIsModalOpen={setIsModalOpen}
+        title="Edit Post"
+      >
+        {post.id && <PostUpdateForm id={post.id} />}
       </Modal>
     </View>
   );
